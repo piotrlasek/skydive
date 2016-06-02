437cd25dc076c11f7b1b0d93fedf2106a2cc69f1
Piotr Lasek <piotr.lasek@gmail.com>
2016-06-02 12:30:48 -0400

    Adding new folder with sql code for creating cubed pyramids and pi-cubes

---
+++ sql/picube/create_space_layer.sql
@@ -0,0 +1,33 @@
+﻿-- ------------------------------------------------------------------
+DROP FUNCTION IF EXISTS CREATE_SPACE_LAYER(SPACE_LAYER_NUM INT) CASCADE;
+-- ------------------------------------------------------------------
+CREATE FUNCTION CREATE_SPACE_LAYER(SPACE_LAYER_NUM INT) RETURNS INTEGER AS $$
+    DECLARE RESULT BIGINT;
+    BEGIN
+        INSERT INTO
+            CUBED_PYRAMID
+        SELECT
+            SPACE_LAYER_NUM,
+            1,
+            CAST(TILE_X / 2 AS BIGINT) AS TILE_X_GROUP,
+            CAST(TILE_Y / 2 AS BIGINT) AS TILE_Y_GROUP,
+            TIME AS TIME_GROUP,
+            SUM(CNT) AS CNT,
+            SUM(CNT_LOG) AS CNT_LOG
+        FROM
+            CUBED_PYRAMID
+        WHERE
+            SPACE_LAYER = SPACE_LAYER_NUM - 1 AND
+            TIME_LAYER = 1
+        GROUP BY
+            TILE_X_GROUP,
+            TILE_Y_GROUP,
+            TIME_GROUP;
+
+        SELECT COUNT(*) FROM PI_CUBE
+            WHERE SPACE_LAYER = SPACE_LAYER_NUM AND
+            TIME_LAYER = 1 INTO RES;
+
+        RETURN RES;
+    END;
+$$ LANGUAGE plpgsql;
---
+++ sql/picube/init_pi_cube.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO PI_CUBE
