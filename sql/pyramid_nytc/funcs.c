4e2dda8e8c6ecefe31c931cb54b0206dd1d660bb
Piotr Lasek <piotr.lasek@gmail.com>
2017-08-08 01:44:08 -0400

    adding logs

--- sql/pyramid_nytc/funcs.c
+++ sql/pyramid_nytc/funcs.c
@@ -7,8 +7,7 @@
 PG_MODULE_MAGIC; 
 #endif 
 
-uint64_t morton2d(uint32_t x, uint32_t y)
-{
+uint64_t morton_enc(uint32_t x, uint32_t y) {
     uint64_t d = 0;
     x = (x | (x << 16)) & 0x0000FFFF0000FFFF;
     x = (x | (x << 8)) & 0x00FF00FF00FF00FF;
@@ -26,28 +25,31 @@ uint64_t morton2d(uint32_t x, uint32_t y)
     return d;
 }
 
-// method to seperate bits from a given integer 3 positions apart
-uint64_t splitBy3(unsigned int a){
-    uint64_t x = a & 0x1fffff; // we only look at the first 21 bits
-    x = (x | x << 32) & 0x1f00000000ffff;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
-    x = (x | x << 16) & 0x1f0000ff0000ff;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
-    x = (x | x << 8) & 0x100f00f00f00f00f; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
-    x = (x | x << 4) & 0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
-    x = (x | x << 2) & 0x1249249249249249;
-    return x;
-}
- 
-uint64_t mortonEncode_magicbits(unsigned int x, unsigned int y, unsigned int z){
-    uint64_t answer = 0;
-    answer |= splitBy3(x) | splitBy3(y) << 1 | splitBy3(z) << 2;
-    return answer;
+
+
+uint32_t Compact1By1(uint64_t x) {
+   x &= 0x55555555;                  // x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+   x = (x ^ (x >>  1)) & 0x33333333; // x = --fe --dc --ba --98 --76 --54 --32 --10
+   x = (x ^ (x >>  2)) & 0x0f0f0f0f; // x = ---- fedc ---- ba98 ---- 7654 ---- 3210
+   x = (x ^ (x >>  4)) & 0x00ff00ff; // x = ---- ---- fedc ba98 ---- ---- 7654 3210
+   x = (x ^ (x >>  8)) & 0x0000ffff; // x = ---- ---- ---- ---- fedc ba98 7654 3210
+   return x;
 }
 
-/* by value */
-         
-int
-add_one(int arg)
-{
-    int64_t i2 = 0x0000444400004444LL;
-    return arg + 1;
+/**
+*
+* @param code
+* @return
+*/
+uint32_t morton_dec_x(uint64_t code) {
+    return Compact1By1(code >> 0);
+}
+
+/**
+*
+* @param code
+* @return
+*/
+uint32_t morton_dec_y(uint64_t code) {
+    return Compact1By1(code >> 1);
 }
