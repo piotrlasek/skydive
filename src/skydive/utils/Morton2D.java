5e7d6eb6b947800a1f28e069d46fcfac06aeb413
Piotr Lasek <piotr.lasek@gmail.com>
2017-09-18 10:58:42 -0400

    fixing zooatlevel

--- src/skydive/utils/Morton2D.java
+++ src/skydive/utils/Morton2D.java
@@ -76,11 +76,74 @@ public class Morton2D {
         return Compact1By1(code >> 1);
     }
 
+    /**
+     *
+     * @param dim
+     * @param deepest
+     * @param level
+     * @param zoo
+     * @return
+     */
+    public static double zooAtLevelA(int dim, int deepest, int level, long zoo) {
+        // return floor(zoo / power(power(CAST((2) as bigint), dim), abs(deepest - level)));
+        double b = Math.abs(deepest - level);
+        double a = Math.pow(2, dim);
+        double p = Math.pow(a, b);
+        double zal = zoo / p;
+        //Morton2D.log.info("a: " + a + ", b: " + b + ", p: " + p + ", zal: " + Math.floor(zal));
+        //Morton2D.log.info("" + zoo + " / Math.pow(" + "Math.pow(2, " + dim + "), Math.abs(" + deepest + " - " + level + "))");
+
+        return Math.floor(zal);
+    }
+    /**
+     *
+     * @param dim
+     * @param deepest
+     * @param level
+     * @param zoo
+     * @return
+     */
+    public static long zooAtLevelB(int dim, int deepest, int level, long zoo) {
+
+        long tzoo1 = zoo;
+        long tzoo2 = zoo;
+        long tzoo3 = zoo;
+
+        int div = (int) Math.pow(4, deepest-level);
+        tzoo3 = tzoo3 / div;
+
+        System.out.println("------------");
+        for (int i = 0; i < deepest - level; i++) {
+           tzoo1 = (tzoo1 / 4);
+           tzoo2 = (long) Math.floor(tzoo2 /4);
+        }
+
+        System.out.println(">" + tzoo1);
+        System.out.println(">" + tzoo2);
+        System.out.println(">" + tzoo3);
+
+        return (long) Math.floor(tzoo2);
+    }
+
+    /**
+     *
+     * @param dim
+     * @param deepest
+     * @param zooA
+     * @param zooB
+     * @return
+     */
+    public static double adjAtLevel2 (int dim, int deepest, long zooA, long zooB) {
+        double dm = dim * Math.log(2);
+        long axb = zooA ^ zooB;
+        double laxb = Math.log(axb);
+        double laxbdm = laxb / dm;
+        return deepest - Math.floor(laxbdm);
+    }
 
 
     public static void main(String[] args) {
-        long zoo;
-
+        /*(long zoo;
         zoo = Morton2D.morton2d(0,0);
         Morton2D.log.info(zoo);
         zoo = Morton2D.morton2d(1,0);
@@ -93,10 +156,26 @@ public class Morton2D {
         Morton2D.log.info(zoo);
         zoo = Morton2D.morton2d(8,9);
         Morton2D.log.info(zoo);
-
-
         zoo = Morton2D.morton2d(2146941188, 2147097832);
         Morton2D.log.info(zoo);
+        */
+        Morton2D.log.info("---------------------");
+        //Morton2D.log.info(Morton2D.zooAtLevelA(2, 31, 18, 1072178917556894766L));
+        Morton2D.log.info(Morton2D.zooAtLevelB(2, 31, 18, 1072178917556894766L));
+        Morton2D.log.info("---------------------");
+        //Morton2D.log.info(Morton2D.zooAtLevelA(2, 31, 18, 1072178917613240256L));
+        Morton2D.log.info(Morton2D.zooAtLevelB(2, 31, 18, 1072178917613240256L));
+        Morton2D.log.info("---------------------");
 
+        /*Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 1072178917556894766L, 1072178917613240256L));
+        Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 31, 32));
+        Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 127, 128));
+        Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 126, 132));*/
+        /*Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 67108800, 10763310));
+        Morton2D.log.info(Morton2D.zooAtLevelA(2, 31, 18, 67108800));
+        Morton2D.log.info(Morton2D.zooAtLevelB(2, 31, 18, 67108800));
+        Morton2D.log.info(Morton2D.zooAtLevelA(2, 31, 18, 10763310));
+        Morton2D.log.info(Morton2D.zooAtLevelB(2, 31, 18, 10763310));*/
+        //Morton2D.log.info(Morton2D.adjAtLevel2(2, 31, 0, 3));
     }
 }
