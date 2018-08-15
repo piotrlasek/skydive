1bbafcda445a7d2a48f2e1568c5151dfdd494237
Piotr Lasek <piotr.lasek@gmail.com>
2018-08-15 13:03:13 -0400

    removing pbs function

--- sql/pyramid_nytc/Pyramid-psql/pop-ByStratum.sql
+++ sql/pyramid_nytc/Pyramid-psql/pop-ByStratum.sql
@@ -15,65 +15,6 @@
 
 drop function if exists popbystratum() cascade;
 
-drop function if exists pbs() cascade;
-
-create or replace function pbs() returns table
-    (lev_ smallint, zoo_ bigint, base_ integer,
-    lft_ smallint, rght_ smallint, point_ bigint, row_ bigint,
-    up_ bigint) as $$
-    declare dim_      smallint default  2;
-    declare maxDepth_ smallint default 31;
-    declare stratum_  smallint;
-
-    begin
-        stratum_ = 19; --(select max(lev) from Pyramid);
-	    RAISE NOTICE 'Level %.', stratum_;
-
-        return query
-            with 
-                SuperCell(lev, zoo, up, lft, rght, point) as (
-                select
-                   lev, zoo,
-                   zooAtLevel(
-                       cast((dim_) as smallint), cast((maxDepth_) as
-                       smallint), cast((lev - 1) as smallint), cast((zoo)
-                       as bigint)),
-                   lft, rght, point
-                from Pyramid
-                where lev = stratum_ 
-                -- 1072178917556894766
-                ),
-                AggrCell (lev, zoo, up, row, lft, rght, point) as (
-                    select  lev, 
-                            --min(zoo) over (partition by up), up,
-                            zoo,
-                            up,
-                            row_number() over (
-                                    partition by up
-                                    order by zoo asc),
-                            min(lft)   over (partition by up),
-                            min(rght)  over (partition by up),
-                            sum(point) over (partition by up)
-                    from SuperCell[[
-                )
-            select  case
-                        when lft > rght then lft-1
-                        else                 rght-1
-                    end,   -- the level of this new tile
-                    zoo,   -- its zoo
-                    lev,   -- its base level, where this cell was created
-                    lft,   -- touches lft neighbour tile at this level
-                    rght,  -- touches rght neighbour tile at this level
-                    point, -- aggregated number of points in new tile
-                    row,
-                    up
-            from AggrCell
-             where row = 1;
-    end;
-$$ language plpgsql;
-
--- select popbystratum();
-
 create function popbystratum() returns int as $$
     declare dim_      smallint default  2;
     declare maxDepth_ smallint default 31;
